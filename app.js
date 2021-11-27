eb9c29a9ec636e1aa998d870dfa14aa4382b0f66
Coder-TheBeJIIHiu <83647537+Coder-TheBeJIIHiu@users.noreply.github.com>
2021-11-27 17:49:54 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ console.log('-------------------------------')
 console.log('	Мирик Топ	') 
 console.log('-------------------------------')
 console.log('')
+var express = require('express');
+var app     = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const reklama = [
 	{
